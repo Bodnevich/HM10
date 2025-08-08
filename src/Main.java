a8a3b987b64794f82fd4cf496bdde470ebd6e620
Bodnevich <bodnevichkristina@mail.ru>
2025-08-08 22:27:52 +0300

    HM10 исп

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1");
 
-        String firstName = "Ivanov";
-        String midlleName = "Ivan";
-        String lastName = "Ivanovich";
-        String fullName = firstName + " " + midlleName + " " + lastName;
+        String firstName = "Ivan";
+        String midlleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + midlleName;
         System.out.println("Ф. И. О. сотрудника — " + fullName);
 
         System.out.println("Задача 2");

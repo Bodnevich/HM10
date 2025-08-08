42f1eab3307b6678ac6857770738bfd01c6dbaa4
Bodnevich <bodnevichkristina@mail.ru>
2025-08-08 20:41:49 +0300

    HM10 исп

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задача 1");
+
+        String firstName = "Ivanov";
+        String midlleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + midlleName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("Задача 2");
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName);
+
+        System.out.println("Задача 3");
+        String fullNameNew = "Иванов Семён Семёнович";
+        fullNameNew = fullNameNew.replace('ё','е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameNew);
     }
 }
